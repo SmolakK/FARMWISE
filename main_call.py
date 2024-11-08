f5e58f0a14967c36b15083ae2f886926358509d5
SmolakK <kamil.smolak@upwr.edu.pl>
2024-11-08 14:56:41 +0100

    Main call as async call + changes
    Added async call, timeout rule, and ability to separate APIs

--- main_call.py
+++ main_call.py
@@ -3,14 +3,16 @@ from utils.overlap_checks import spatial_ranges_overlap, time_ranges_overlap
 import importlib
 import pandas as pd
 import logging
+import asyncio
 
 logger = logging.getLogger(__name__)
 
 
-async def read_data(bounding_box, level, time_from, time_to, factors):
+async def read_data(bounding_box, level, time_from, time_to, factors, separate_api=False, timeout=600):
     """
     Main data reading call - combines different APIs which overlap with the requested area and time range.
 
+    :param separate_api: If True APIs are stored in separate columns and not averaged
     :param bounding_box: A tuple containing the geographical coordinates (N, S, E, W) of the area for which data is requested.
                          Format: (North, South, East, West) in decimal degrees.
     :param level: S2Cell level.
@@ -36,12 +38,25 @@ async def read_data(bounding_box, level, time_from, time_to, factors):
         temporal_overlap = time_ranges_overlap((time_from, time_to), api_time_range)  # Check temporal overlap
         data_overlap = set(factors).intersection(api_data_range)  # Check data overlap
         if spatial_overlap and temporal_overlap and len(data_overlap) > 0:  # If overlaps
-            module = importlib.import_module(api_name)  # Import the proper module
-            # Read data from the module (parameters are the same for all read_data() functions)
-            api_response_data = module.read_data(spatial_range=bounding_box, time_range=(time_from, time_to),
-                                                 data_range=factors, level=level)
-            if api_response_data is not None:
-                data_storage.append(api_response_data)
-                logger.info(f'Data retrieved from {api_name}')
-    data_storage = pd.concat(data_storage,axis=1)  # TODO: separate or together
-    return data_storage
+            try:
+                module = importlib.import_module(api_name)  # Import the proper module
+                # Read data from the module (parameters are the same for all read_data() functions)
+                api_response_data = await asyncio.wait_for(module.read_data(spatial_range=bounding_box, time_range=(time_from, time_to),
+                                                     data_range=factors, level=level),
+                                                           timeout=timeout)
+                if api_response_data is not None:
+                    api_name_suffix = api_name.split('.')[-1]
+                    if separate_api:
+                        api_response_data = api_response_data.add_suffix(' ({})'.format(api_name_suffix))
+                    data_storage.append(api_response_data)
+                    logger.info(f'Data retrieved from {api_name_suffix}')
+            except asyncio.TimeoutError:
+                logger.error(f'Request to {api_name_suffix} timed out')
+            except Exception as e:
+                logger.error(f'Failed to retrieve data from {api_name}: {e}')
+    if data_storage:
+        return pd.concat(data_storage, axis=1)
+    else:
+        return pd.DataFrame()
+
+asyncio.run(read_data((50, 49, 20, 18), 10, '2023-10-10', '2023-10-12', ['temperature'], separate_api=False))
