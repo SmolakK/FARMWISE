f6fbac5cc3d5a99ed907158ea608d24f8b75eadf
SmolakK <kamil.smolak@upwr.edu.pl>
2024-11-12 18:29:13 +0100

    Update main_call.py
    Add new flags for users

--- main_call.py
+++ main_call.py
@@ -1,5 +1,6 @@
 from mappings.data_source_mapping import API_PATH_RANGES
 from utils.overlap_checks import spatial_ranges_overlap, time_ranges_overlap
+from utils.interpolate_data import interpolate
 import importlib
 import pandas as pd
 import logging
@@ -8,10 +9,13 @@ import asyncio
 logger = logging.getLogger(__name__)
 
 
-async def read_data(bounding_box, level, time_from, time_to, factors, separate_api=False, timeout=600):
+async def read_data(bounding_box, level, time_from, time_to, factors, separate_api=False, timeout=600, interpolation=False):
     """
     Main data reading call - combines different APIs which overlap with the requested area and time range.
 
+    :param interpolation: If true, interpolation is applied to the resulting data. Especially useful for maps and
+    high data resolutions.
+    :param timeout: Timeout for each API after which the process will skip this API.
     :param separate_api: If True APIs are stored in separate columns and not averaged
     :param bounding_box: A tuple containing the geographical coordinates (N, S, E, W) of the area for which data is requested.
                          Format: (North, South, East, West) in decimal degrees.
@@ -44,19 +48,30 @@ async def read_data(bounding_box, level, time_from, time_to, factors, separate_a
                 api_response_data = await asyncio.wait_for(module.read_data(spatial_range=bounding_box, time_range=(time_from, time_to),
                                                      data_range=factors, level=level),
                                                            timeout=timeout)
-                if api_response_data is not None:
+                if isinstance(api_response_data, pd.DataFrame):
                     api_name_suffix = api_name.split('.')[-1]
                     if separate_api:
-                        api_response_data = api_response_data.add_suffix(' ({})'.format(api_name_suffix))
+                        api_response_data = api_response_data.add_suffix(f' ({api_name_suffix})')
                     data_storage.append(api_response_data)
                     logger.info(f'Data retrieved from {api_name_suffix}')
+
             except asyncio.TimeoutError:
                 logger.error(f'Request to {api_name_suffix} timed out')
             except Exception as e:
                 logger.error(f'Failed to retrieve data from {api_name}: {e}')
+
+    # Concatenate data if any DataFrames were retrieved
     if data_storage:
-        return pd.concat(data_storage, axis=1)
+        try:
+            combined_data = pd.concat(data_storage, axis=1)
+            if interpolation:
+                combined_data = interpolate(combined_data, bounding_box, level)
+            return combined_data
+        except Exception as e:
+            logger.error(f'Error concatenating data: {e}')
+            return pd.DataFrame()  # Return an empty DataFrame if concatenation fails
     else:
+        logger.warning("No data retrieved from available APIs")
         return pd.DataFrame()
 
-asyncio.run(read_data((50, 49, 20, 18), 10, '2023-10-10', '2023-10-12', ['temperature'], separate_api=False))
+asyncio.run(read_data((50, 49, 20, 18), 10, '2023-10-10', '2023-10-12', ['hydraulic conductivity'], separate_api=False, interpolation=True))
