5d87987bfd16522c290b44bb5a7551003d24ea61
SmolakK <kamil.smolak@upwr.edu.pl>
2024-11-19 21:54:10 +0100

    Update coordinates_to_cells.py

--- utils/coordinates_to_cells.py
+++ utils/coordinates_to_cells.py
@@ -19,11 +19,12 @@ def prepare_coordinates(coordinates, spatial_range, level):
     """
     Prepare coordinates for data retrieval, limiting them to the specified spatial range and assigning S2Cell IDs.
 
+    :param coordinates: Coordinates to be transformed
     :param spatial_range: A tuple containing the spatial range (N, S, E, W) defining the bounding box.
     :param level: S2Cell level.
     :return: DataFrame containing coordinates within the specified spatial range and their corresponding S2Cell IDs.
     """
-    coords = coordinates.copy() # to prevent the warning: "A value is trying to be set on a copy of a slice from a DataFrame" at the .apply line below
+    coords = coordinates.copy()  # to prevent the warning about the copy-setting
     coords.lat = coords.lat.astype('float32')
     coords.lon = coords.lon.astype('float32')
     coords = _limit_coordinates(spatial_range=spatial_range, coordinates=coords)
