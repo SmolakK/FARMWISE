5cfe63a297359c3b91044c1e1d609eead899b13e
SmolakK <kamil.smolak@upwr.edu.pl>
2024-12-03 09:40:30 +0100

    Gios Utils moved to separate module
    done because of testing

--- API_readers/gios/gios_api_stations.py
+++ API_readers/gios/gios_api_stations.py
@@ -1,34 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
 from tqdm import tqdm
 from utils.name_to_coordinates import get_coordinates
-
-
-def generate_urls(base_url, start=2, end=32, step=2):
-    # Generate a list of URLs based on a range of numbers
-    return [f"{base_url}{i:02d}" for i in range(start, end + 1, step)]
-
-
-def fetch_and_parse(url):
-    # Fetch URL content and parse it with BeautifulSoup.
-    response = requests.get(url)
-    if response.status_code == 200:
-        return BeautifulSoup(response.content, 'html.parser')
-    return None  # Return None if response is not OK
-
-
-def extract_data(soup):
-    # Extract data from parsed HTML soup.
-
-    if soup is None:
-        return []
-    spans = soup.find_all('span', class_='w1')
-    data = [{'id': int(spans[i].get_text(strip=True)),
-             'name': spans[i + 1].get_text(strip=True) + ', Poland'}
-            for i in range(0, len(spans), 2)]
-    return data
-
+from API_readers.gios.gios_utils import generate_urls, fetch_and_parse, extract_data
 
 # Setup base URL
 base_url = 'https://www.gios.gov.pl/chemizm_gleb/index.php?mod=pomiary&w='
