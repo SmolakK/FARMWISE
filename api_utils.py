e1804814789384d335d0fd4ba160b95882f9e439
SmolakK <kamilsmolak24@gmail.com>
2025-01-16 09:50:45 +0100

    Update api_utils.py

--- api_utils.py
+++ api_utils.py
@@ -1,18 +1,22 @@
 import os
 import time
+from logging_config import logger
 
 
 def cleanup_old_files(folder_path, max_age_in_seconds):
     """
     Deletes files older than `max_age_in_seconds` in the specified `folder_path`.
     """
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        if os.path.isfile(file_path) and '.csv' in filename:
-            file_age = time.time() - os.path.getmtime(file_path)
-            if file_age > max_age_in_seconds:
-                os.remove(file_path)
-                print(f"Deleted {file_path}")
+    try:
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            if os.path.isfile(file_path) and '.csv' in filename:
+                file_age = time.time() - os.path.getmtime(file_path)
+                if file_age > max_age_in_seconds:
+                    os.remove(file_path)
+                    print(f"Deleted {file_path}")
+    except FileNotFoundError:
+        logger.info("File already deleted")
 
 
 def secure_filename(filename):
