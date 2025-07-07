eddfc0c378eb73efbebced23318c293315bf08a3
SmolakK <kamil.smolak@upwr.edu.pl>
2025-07-07 15:56:38 +0200

    Country bboxes

---
+++ utils/country_bboxes.py
@@ -0,0 +1,20 @@
+import json
+
+jpath = f'country_codes'
+with open(jpath,'r') as jfile:
+    country_json = json.load(jfile)
+
+COUNTRY_BBOXES = {}
+
+for country in country_json.values():
+    region = country.get("region", "")
+    if region != "Europe":
+        continue  # Skip non-European countries
+
+    name = country["name"]
+    bbox = country["boundingBox"]
+    north = bbox["ne"]["lat"]
+    south = bbox["sw"]["lat"]
+    east = bbox["ne"]["lon"]
+    west = bbox["sw"]["lon"]
+    COUNTRY_BBOXES[name] = (north, south, east, west)
