95ebb5a769e024c3564383afc49f37824617c303
SmolakK <kamil.smolak@upwr.edu.pl>
2025-07-07 16:35:40 +0200

    Use country names to pick places

--- utils/country_bboxes.py
+++ utils/country_bboxes.py
@@ -1,20 +1,24 @@
 import json
 
-jpath = f'country_codes'
-with open(jpath,'r') as jfile:
-    country_json = json.load(jfile)
 
-COUNTRY_BBOXES = {}
+def return_country_bboxes():
+    jpath = f'utils\country_codes'
+    with open(jpath,'r') as jfile:
+        country_json = json.load(jfile)
 
-for country in country_json.values():
-    region = country.get("region", "")
-    if region != "Europe":
-        continue  # Skip non-European countries
+    COUNTRY_BBOXES = {}
 
-    name = country["name"]
-    bbox = country["boundingBox"]
-    north = bbox["ne"]["lat"]
-    south = bbox["sw"]["lat"]
-    east = bbox["ne"]["lon"]
-    west = bbox["sw"]["lon"]
-    COUNTRY_BBOXES[name] = (north, south, east, west)
+    for country in country_json.values():
+        region = country.get("region", "")
+        if region != "Europe":
+            continue  # Skip non-European countries
+
+        name = country["name"]
+        bbox = country["boundingBox"]
+        north = bbox["ne"]["lat"]
+        south = bbox["sw"]["lat"]
+        east = bbox["ne"]["lon"]
+        west = bbox["sw"]["lon"]
+        COUNTRY_BBOXES[name] = (north, south, east, west)
+
+    return COUNTRY_BBOXES
