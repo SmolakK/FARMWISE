2270df0fdffbaa3492b8656492e6d426ac4ed18c
dominikteodorczyk <dominik.teodorczyk1@gmail.com>
2026-01-26 14:38:24 +0100

    tests for eea wrapper

---
+++ tests/test_eea.py
@@ -0,0 +1,9 @@
+import pytest
+from unittest.mock import patch, AsyncMock, MagicMock
+import numpy as np
+import pandas as pd
+from API_readers.eea.eea_wrapper import read_data
+
+@pytest.mark.asyncio
+async def test_read_data():
+    pass
