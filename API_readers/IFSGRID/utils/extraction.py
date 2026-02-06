fe4c22525eae2c687ff312f85f3ea84689e7f846
dominikteodorczyk <dominik.teodorczyk1@gmail.com>
2026-02-06 15:45:40 +0100

    create funcs for data extraction

---
+++ API_readers/IFSGRID/utils/extraction.py
@@ -0,0 +1,14 @@
+import geopandas as gpd
+import numpy as np
+
+def create_vector_box(shp:gpd.GeoDataFrame,bbox:tuple) -> gpd.GeoDataFrame:
+    pass
+
+def create_grid(bbox:tuple, resolution:int=100):
+    pass
+
+def extract_vector_to_grid(shp:gpd.GeoDataFrame,grid:gpd.GeoDataFrame):
+    pass
+
+def interpolate_grids(solutons):
+    pass
