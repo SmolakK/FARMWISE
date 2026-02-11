d9d8f4b808a7907ae6dc3e1a0d160783fbfb38a2
dominikteodorczyk <dominik.teodorczyk1@gmail.com>
2026-02-11 17:25:44 +0100

    extraction prototype

--- API_readers/IFSGRID/utils/extraction.py
+++ API_readers/IFSGRID/utils/extraction.py
@@ -1,14 +1,18 @@
 import geopandas as gpd
 import numpy as np
+import pandas as pd
 
-def create_vector_box(shp:gpd.GeoDataFrame,bbox:tuple) -> gpd.GeoDataFrame:
-    pass
+def extract_values(shp_path:str, factor_feature:str, bbox:tuple) -> pd.DataFrame:
+    
+    with gpd.read_file(shp_path) as src:
+        gdf = src.to_crs(epsg=4326)
 
-def create_grid(bbox:tuple, resolution:int=100):
-    pass
+    gdf_bbox = gdf[gdf.geometry.intersects(bbox)]
+    gdf_bbox['geometry'] = gdf_bbox["geometry"].centroid
 
-def extract_vector_to_grid(shp:gpd.GeoDataFrame,grid:gpd.GeoDataFrame):
-    pass
+    feature_value = pd.DataFrame()
+    feature_value['lon'] = gdf_bbox.geometry.x
+    feature_value['lat'] = gdf_bbox.geometry.y
+    feature_value['factor'] = gdf_bbox[factor_feature]
 
-def interpolate_grids(solutons):
-    pass
+    return feature_value
