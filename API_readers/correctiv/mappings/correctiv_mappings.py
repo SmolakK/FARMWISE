8e275b5471707dffd0e84fa4108bc4495351121d
dominikteodorczyk <dominik.teodorczyk1@gmail.com>
2026-04-21 16:11:44 +0200

    correctiv mappings

---
+++ API_readers/correctiv/mappings/correctiv_mappings.py
@@ -0,0 +1,12 @@
+# list of parameters in the parameter column
+PARAMETER_VALUES = ['min_gwl','mean_gwl','max_gwl']
+
+# selection list for parameters
+PARAMETER_SELECTION = []
+
+# aliases for the data fields
+DATA_ALIASES = {}
+
+
+# global mapping of parameter codes to descriptions and units
+GLOBAL_MAPPING = {}
