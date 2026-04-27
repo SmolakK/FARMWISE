5d1863422ea63ae46cf127d67b525282ca87f156
dominikteodorczyk <dominik.teodorczyk1@gmail.com>
2026-04-27 12:49:57 +0200

    ECV2 valueas expl.

--- API_readers/EuroCropV2/mappings/EuroCropV2_mappings.py
+++ API_readers/EuroCropV2/mappings/EuroCropV2_mappings.py
@@ -1,1 +1,7 @@
 PARAMETERS_SELECTION = ['c','cf']
+
+# global mapping of parameter codes to descriptions and units
+GLOBAL_MAPPING = {
+    'c': 'Original cultivation code in the annual GSA layer',
+    'cf': 'Parcel ID in the annual GSA layer'
+}
